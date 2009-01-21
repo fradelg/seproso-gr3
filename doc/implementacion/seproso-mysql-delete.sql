ad6c929c2358084fe121ff95866e3c616861940a
dante.aureon <dante.aureon@810df87c-b558-11dd-8610-bdbd52298f7b>
2009-01-21 16:37:56 +0000

    -- Script borrar DataBase (falta probarlo)

---
+++ doc/implementacion/seproso-mysql-delete.sql
@@ -0,0 +1,16 @@
+DROP TABLE `Trabajador_has_Actividad`;
+DROP TABLE `PeriodoVacacional`;
+DROP TABLE `Configuracion`;
+DROP TABLE `Participacion`;
+DROP TABLE `Rol`;
+DROP TABLE `TipoActividad`;
+DROP TABLE `Actividad`;
+DROP TABLE `Trabajador`;
+DROP TABLE `Usuario`;
+DROP TABLE `Fase`;
+DROP TABLE `Modelo`;
+DROP TABLE `RegistroTrabajo`;
+DROP TABLE `Artefacto`;
+DROP TABLE `Proyecto`;
+
+
