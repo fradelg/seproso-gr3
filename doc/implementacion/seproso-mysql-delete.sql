d5a68543fcc7ebb4622524c4f9e721e4054f8348
dante.aureon <dante.aureon@810df87c-b558-11dd-8610-bdbd52298f7b>
2009-01-31 11:57:31 +0000

    -- Script nuevo de borrado de tablas.

--- doc/implementacion/seproso-mysql-delete.sql
+++ doc/implementacion/seproso-mysql-delete.sql
@@ -1,17 +1,18 @@
-DROP TABLE `Trabajador_has_Actividad`;
-DROP TABLE `PeriodoVacacional`;
-DROP TABLE `Configuracion`;
-DROP TABLE `Participacion`;
-DROP TABLE `Trabajador`;
-DROP TABLE `RegistroTrabajo`;
-DROP TABLE `Proyecto`;
-DROP TABLE `Rol`;
-DROP TABLE `Actividad`;
-DROP TABLE `TipoActividad`;
-DROP TABLE `Usuario`;
-DROP TABLE `Fase`;
-DROP TABLE `Modelo`;
-DROP TABLE `Artefacto`;
+DROP TABLE IF EXISTS `Actividad_Predecesora`;
+DROP TABLE IF EXISTS `Trabajador_has_Actividad`;
+DROP TABLE IF EXISTS `PeriodoVacacional`;
+DROP TABLE IF EXISTS `Configuracion`;
+DROP TABLE IF EXISTS `Participacion`;
+DROP TABLE IF EXISTS `Trabajador`;
+DROP TABLE IF EXISTS `RegistroTrabajo`;
+DROP TABLE IF EXISTS `Proyecto`;
+DROP TABLE IF EXISTS `Rol`;
+DROP TABLE IF EXISTS `Actividad`;
+DROP TABLE IF EXISTS `TipoActividad`;
+DROP TABLE IF EXISTS `Usuario`;
+DROP TABLE IF EXISTS `Fase`;
+DROP TABLE IF EXISTS `Modelo`;
+DROP TABLE IF EXISTS `Artefacto`;
 
 
 
